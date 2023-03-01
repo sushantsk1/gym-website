0cf606d2af85a19209e9942682362f7d7126bf66
sushant kulkarni <83342285+sushantsk1@users.noreply.github.com>
2023-03-01 14:14:06 +0530

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>GYMSTER - Gym HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Rubik&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    
+<?php include('include/header.php');?>
+
+    <!-- Hero Start -->
+    <div class="container-fluid bg-primary p-5 bg-hero mb-5">
+        <div class="row py-5">
+            <div class="col-12 text-center">
+                <h1 class="display-2 text-uppercase text-white mb-md-4">Services</h1>
+                <a href="index.php" class="btn btn-primary py-md-3 px-md-5 me-3">Home</a>
+                
+            </div>
+        </div>
+    </div>
+    <!-- Hero End -->
+
+
+    <!-- Blog Start -->
+    <div class="container-fluid p-5">
+        <div class="row g-5">
+            <!-- Blog list Start -->
+            <div class="col-lg-12">
+                <div class="row g-5">
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/blog-1.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Weight lifting</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/services/cardio.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Cardio</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/blog-3.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Personal training</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/bodybuilding.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Body building</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/zoomba.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Zumba</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/crossfit.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Cross-fit</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/locker.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Lockers</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/sauna.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Sauna</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="blog-item">
+                            <div class="position-relative overflow-hidden rounded-top">
+                                <img class="img-fluid" src="img/Services/swimming.jpg" alt="">
+                            </div>
+                            <div class="bg-dark d-flex align-items-center rounded-bottom p-4">
+                                
+                                <a class="h5 text-uppercase text-light" href="">Swimming Pools</h4></a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <!-- Blog list End -->
+
+            
+
+
+     <?php include ('include/footer.php');?>
+
+   
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>GYMSTER - Gym HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Rubik&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    
+<?php include ('include/header.php');?>
+
+    <!-- Hero Start -->
+    <div class="container-fluid bg-primary p-5 bg-hero mb-5">
+        <div class="row py-5">
+            <div class="col-12 text-center">
+                <h1 class="display-2 text-uppercase text-white mb-md-4">Contact</h1>
+                <a href="index.php" class="btn btn-primary py-md-3 px-md-5 me-3">Home</a>
+               
+            </div>
+        </div>
+    </div>
+    <!-- Hero End -->
+
+
+    <!-- Contact Start -->
+    <div class="container-fluid p-5">
+        <div class="mb-5 text-center">
+            <h5 class="text-primary text-uppercase">Contact Us</h5>
+            <h1 class="display-3 text-uppercase mb-0">Get In Touch</h1>
+        </div>
+        <div class="row g-5 mb-5">
+            <div class="col-lg-4">
+                <div class="d-flex flex-column align-items-center bg-dark rounded text-center py-5 px-3">
+                    <div class="bg-primary rounded-circle d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 60px;">
+                        <i class="fa fa-map-marker-alt fs-4 text-white"></i>
+                    </div>
+                    <h5 class="text-uppercase text-primary">Visit Us</h5>
+                    <p class="text-secondary mb-0">servy no 184, Hirawadi Rd, above deeplaxmi karyalay, Nashik, Maharashtra 422003</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="d-flex flex-column align-items-center bg-dark rounded text-center py-5 px-3">
+                    <div class="bg-primary rounded-circle d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 80px;">
+                        <i class="fa fa-envelope fs-4 text-white"></i>
+                    </div>
+                    <h5 class="text-uppercase text-primary">Email Us</h5>
+                    <p class="text-secondary mb-0">afitness@gmail.com</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="d-flex flex-column align-items-center bg-dark rounded text-center py-5 px-3">
+                    <div class="bg-primary rounded-circle d-flex align-items-center justify-content-center mb-3" style="width: 60px; height: 80px;">
+                        <i class="fa fa-phone fs-4 text-white"></i>
+                    </div>
+                    <h5 class="text-uppercase text-primary">Call Us</h5>
+                    <p class="text-secondary mb-0"><a href="tel:">+917775062359</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="col-lg-6">
+                <div class="bg-dark p-5">
+                    <form>
+                        <div class="row g-3">
+                            <div class="col-6">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="Your Name" style="height: 55px;">
+                            </div>
+                            <div class="col-6">
+                                <input type="email" class="form-control bg-light border-0 px-4" placeholder="Your Email" style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="Subject" style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <textarea class="form-control bg-light border-0 px-4 py-3" rows="4" placeholder="Message"></textarea>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <iframe class="w-100"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14995.076202725097!2d73.81145817931268!3d20.01820543486894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bddea511982f0e5%3A0x1f6cd8c245b9a789!2sAFITNESS%20gym!5e0!3m2!1sen!2sin!4v1677658547587!5m2!1sen!2sin"
+                    frameborder="0" style="height: 440px; border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+    <?php include ('include/footer.php');?>
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-dark py-3 fs-4 back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
